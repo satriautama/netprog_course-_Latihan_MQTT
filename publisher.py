1b1b0c791fd1bd1fb77f160157b18071029fedc8
I GUSTI NGURAH AGUNG WAHYU SATRIA UTAMA <64734006+satriautama@users.noreply.github.com>
2020-05-16 18:19:24 +0800

    Add files via upload

---
+++ publisher.py
@@ -0,0 +1,40 @@
+# Thanks to: https://techtutorialsx.com/2017/04/14/python-publishing-messages-to-mqtt-topic/
+"""Publish a Topic to MQTT Broker"""
+
+import paho.mqtt.client as mqttClient
+import time
+ 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print('Connected to broker')
+        global Connected                #Use global variable
+        Connected = True                #Signal connection 
+    else:
+        print('Connection failed')
+ 
+Connected = False   #global variable for the state of the connection
+
+# After you successfully the publisher and subscribe code, 
+# try to create your own MQTT broker by create an account at cloudmqtt.com
+broker_address= 'mqtt.eclipse.org'
+port = 1883 # do not type string, but change to integer
+#username = 'test'
+#password = '123'
+ 
+client = mqttClient.Client('Python')               #create new instance
+#client.username_pw_set(username, password=password) #set username and password
+client.on_connect= on_connect                      #attach function to callback
+client.connect(broker_address, port=port)          #connect to broker
+ 
+client.loop_start()        #start the loop
+ 
+while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+ 
+try:
+    while True: 
+        client.publish('myhome/170010214 satria utama/kitchen')
+ 
+except KeyboardInterrupt:
+    client.disconnect()
+    client.loop_stop()
---
+++ subscriber.py
@@ -0,0 +1,52 @@
+"""This script is an example of subscriber to CloudMQTT Broker"""
+
+from urllib.parse import urlparse
+import os
+import paho.mqtt.client as mqtt
+
+# Define event callbacks
+def on_connect(mosq, obj, rc):
+    print('Reconnect: {}'.format(str(rc)))
+
+def on_message(mosq, obj, msg):
+    # message = str(msg.payload)
+    print('Topic: {}/{}'.format(msg.topic, msg.payload.decode('utf-8')))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print('Subscribed: {}, QoS: {}'.format(str(mid), str(granted_qos)))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+#username = 'test'
+#password = '123'
+host_url = 'mqtt.eclipse.org'
+host_port = 1883 # do not type string, but change to integer
+
+mqttc = mqtt.Mosquitto()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+
+# Uncomment to enable debug messages
+#mqttc.on_log = on_log
+
+# Parse CLOUDMQTT_URL (or fallback to localhost)
+url_str = os.environ.get('mqtt.eclipse.org', host_url)
+url = urlparse(url_str)
+
+# Connect
+#username = 'test'
+#password = '123'
+#mqttc.username_pw_set(username, password)
+mqttc.connect('mqtt.eclipse.org', 1883)
+
+# Subscribe to a topic
+mqttc.subscribe('myhome/+/kitchen', 0)
+
+# Continue the network loop, exit when an error occurs
+rc = 0
+while rc == 0:
+    rc = mqttc.loop()
+print('Reconnect: ' + str(rc))
